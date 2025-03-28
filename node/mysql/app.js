ad3313b9d4897202323f088ef8c9beef984147f2
YellowwwA <happiness3651@gmail.com>
2025-03-28 14:57:37 +0900

    node mysql

--- node/mysql/app.js
+++ node/mysql/app.js
@@ -11,7 +11,7 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function (err) {
-    if (err) {
+    if (!err) {
         console.error('Database is connected~!!\n\n');
     } else {
         console.error('Error connecting Database~!!\n\n');
